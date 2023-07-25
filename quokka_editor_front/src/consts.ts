528dc9375dc69fe7805130e9d033e44197bc90a1
Bartosz Sietejko <barto32224@wp.pl>
2023-07-25 17:32:00 +0200

    added login and register forms

---
+++ quokka_editor_front/src/consts.ts
@@ -0,0 +1,6 @@
+export const API_URL = "http://localhost:8100/";
+export const ERRORS = {
+  required: "This field is required",
+  email: "Please provide a valid email address.",
+  passwordLength: "Password must be at least 8 characters long",
+};
